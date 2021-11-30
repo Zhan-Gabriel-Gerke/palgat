23d007eb0044992827e3f0364a028873366b80f7
opilane <opilane@HP-CZC6297B92>
2021-11-30 14:02:57 +0200

    bcvbc

--- module1.py
+++ module1.py
@@ -1,145 +1,73 @@
-def (palg,inimesed):
-    add=input("sisesta nimi: ")
-    inimesed.append(add)
-    add_zp=int(input("sisesta palga: "))
-    palg.append(add_zp)
-    return palg,inimesed
-def zxcv(palg,inimesed):
-    add=input("nimi inemene:  ")
-    inimesed.append(add)
-    add_palg=int(input("palga inimene:  " ))
-    palg.append(add_palg)
-    return palg,inimesed
-    print()
-    print()
-def asdf(palg, inimesed):
-    x=input("Kasutaja nimi on kirjutatud tähtedega - 1 või numbritega - 2? ")
-    if x=="2":
-        i=int(input("sisesta number: "))
-        palg.pop(i-1)
-        inimesed.pop(i-1)
-    elif x=="1":
-        i=0
-        keda=input("sisesta nimi: ")
-        n=len(inimesed)
-        while i<n:
-            if keda==inimesed[i]:
-                inimesed.pop(i)
-                palg.pop(i)
-                n=len(inimesed)
-            else:
-                i+=1
-def kustutamine():
-    keskmin = keskmine(palg)
-    print(keskmin)
-    for i in palg:
-        if i < kesk:
-            index = palg.index(i)
-            palg.pop(index)
-            inimesed.pop(index)
-            print()
-            print()
-def nimi(palg,inimesed):
-    ots_nimi=[]
-    ots_palg=[]
-    palg_keda=0
-    keda=input("sisesta nimi: ")
-    n=len(inimesed)
-    for j in range(n):
-        if inimesed[j]==keda:
-            palg_keda=palg[j]
-            ots_nimi.append(inimesed[j])
-            ots_palg.append(palg_keda)
-            print()
-            print()
-        else:pass
-    return ots_nimi,ots_palg
-    print()
-    print()
-def maksimum(palg,inimesed):
-    m_palgad=[]
-    nimed=[]
-    max_palg=palg[0]
-    kellel=inimesed[0]
-    for p in palg:
-        if p>max_palg:
-            max_palg=p
-            i=palg.index(max_palg)
-            kellel=inimesed[i]
-            print()
-            print()
-    n=palg.count(max_palg)
-    palg_copy=palg.copy()
-    inimesed_copy=inimesed.copy()
-    for i in range(n):
-        j=palg_copy.index(max_palg)
-        m_palgad.append(palg_copy.pop(j))
-        nimed.append(inimesed_copy.pop(j))
-    return m_palgad, nimed
-    print()
-    print()
-def minimum(palg,inimesed):
-    m_palgad=[]
-    nimed=[]
-    min_palg=palg[0]
-    kellel=inimesed[0]
-    for p in palg:
-        if p<min_palg:
-            min_palg=p
-            i=palk.index(min_palg)
-            kellel=inimesed[i]
-    n=palk.count(min_palk)
-    palk_copy=palk.copy()
-    inimesed_copy=inimesed.copy()
-    for i in range(n):
-        j=palk_copy.index(min_palk)
-        m_palgad.append(palk_copy.pop(j))
-        nimed.append(inimesed_copy.pop(j))
-    return m_palgad, nimed
-    print()
-    print()
-def keskmine(palg):
-    summa=0
-    n=len(palg)
-    for p in palg:
-        summa+=p
-    k=summa/n
-    return k
-    print()
-    print()
-def nimi(palg,inimesed):
-    ots_nimi=[]
-    ots_palg=[]
-    palg_keda=0
-    keda=input("sisesta nimi: ")
-    n=len(inimesed)
-    for j in range(n):
-        if inimesed[j]==keda:
-            palg_keda=palg[j]
-            ots_nimi.append(inimesed[j])
-            ots_palg.append(palg_keda)
-            print()
-            print()
-        else:pass
-    return ots_nimi,ots_palg
-    print()
-    print()
-def otsiing_palgad_jargi(inimesed:list,palgad:list):
-    """Tagastame järjend või teksti
-    :rtype var: tulemus
+def add_person ():
+	"""inimese ja palga lisamine
+	"""
+	nimi=input("Siseta nimi: ")
+	palga=input("Siseta palgad: ")
+	with open("TextFile1.txt", "a") as inimesed:
+		inimesed.write(nimi+"\n")	
+	with open("TextFile2.txt", "a") as palgad:
+		palgad.write(palga+"\n")
+def delete_person ():
+	"""Isiku ja palga eemaldamine,Удаление человека и зарплаты 
+	"""
+	f=open("TextFile1.txt", "r")
+	inimesed=[]
+	for stroka in f:
+		inimesed.append(stroka.strip())
+	f.close
+	nimi=input("Siseta nimi: ")
+	if nimi not in inimesed:
+		print("Kas sa tahad lisama nimi ja palgad/?")
+		c=input("Y = jah, N = ei")
+		if c.upper=="Y":
+			add_person()
+		else:
+			pass
+	else:
+		palgad=[]
+		with open("TextFile2.txt", "r") as f1:
+			for stro in f1:
+				palgad.append(stro.strip())
+		a=inimesed.index(nimi)
+		inimesed.pop(a)
+		palgad.pop(a)
+	f=open("TextFile1.txt", "w")
+	for g in inimesed:
+		f.write(g+"\n")
+	f.close()
+	d=open("TextFile2.txt", "w")
+	for i in palgad:
+		d.write(i+"\n")
+	d.close()
+def loe_failist_listisse(file:str)->list:
+    """Loeme tekst failist ja salvesta järjendisse
     """
-    nimi=input("Keda otsime?")
-    for inimene in inimesed:
-        if inimene.upper()==nimi.upper():
-            n=inimesed.cocunt(nimi)
-            print("Inemene on olemas, selline nimi kohtume",n,"korda")
-            b=-1
-            for i in range(n):
-                k=inimesed.index(nimi)
-                palk=palgad[k]
-                b+=K
-                t.append(nimi+str(palk))
- 
-    else:
-        t="Ei ole nimekotjas"
-        return t 
+    file=open(file,"r")
+    list_=[]
+    for stroka in file:
+        list_.append(stroka.strip())
+    file.close()
+    return list_
+def keskmine():
+	"""Kasmise palka leidmine. Kui ta on loetelus, siis näitame kes saab seda kätte
+	:rtype var:
+	"""
+    summa=0
+    for palga in p:
+        summa+=palga
+	kesk=summa/len(p)
+	print(kesk)
+	vahe=0
+	if 0<=p.index(kesk)<len(p)-1:
+		kesk=i[p.index(kesk)]
+		return kesk
+	else:
+		kesk="Puudunud"
+		return kesk
+def suurim_palk(i,p):
+	suurim=max(p)
+	b=p.index(suurim)
+	kellel=i[b]
+	print(suurim)
+	print(kellel)
+	return suurim,kellel
--- palgat.py
+++ palgat.py
@@ -1,38 +1,15 @@
 from module1 import *
-palk=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
-def palgad(p,i):
-    valik=input("Keskmine palk - 1,\nMiinimumpalk - 2,\nMaksimaalne palk - 3,\nKustutada mees - 4,\n \nLisada mees - 5\n")
-    if valik=="1":
-        kesk_palk=round(keskmine(palk),2)
-        print("Keskmine palk on ",kesk_palk)
-        print()
-        print()
-    elif valik=="2":
-        m_palgad,nimi=minimum(palk,inimesed)
-        for n in nimi:
-            print(m_palgad[0], " on saanud ",n)
-            print()
-            print()
-    elif valik=="3":
-        max_palk,kto=maksimum(palk,inimesed)
-        print("Maksimaalne palk ", max_palk, " on saanud ",kto)
-        print()
-        print()
-    elif valik=="4":                                       
-        p,i=asdf(palk,inimesed)
-        print(palk,inimesed)
-        if len(inimesed)==0:
-            print("Tühi list")
-        else:
-            for i in range(len(inimesed)):
-                print(inimesed[i]," saab kätte ", palk[i])
-    elif valik=="5":                                        
-        i=zxcv(palk,inimesed)
-        print("Ajakohastatud nimekirjad: ")
-        print(inimesed)
-        print(palk)
-        print("Lisatud üksus", inimesed[-1])
-        print("Koos palgaga", str(palk[-1])+'€')
+p=loe_failist_listisse("TextFile2.txt")
+i=loe_failist_listisse("TextFile1.txt")
 while True:
-    palgad(palk,inimesed)
+	a=input("Funktsioonid: Add users-1\n delete users-2\n keskmine-3\n suurim palk-4")
+	if a=="1":
+		add_person ()
+	elif a=="2":
+		delete_person()
+	elif a=="3":
+		 keskmine()
+	elif a=="4":
+		suurim_palk(i,p)
+	else:
+		break
